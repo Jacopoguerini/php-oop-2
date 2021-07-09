59ff96de35a54fce7a771c8f5bb70f670a4a0013
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-09 17:48:20 +0200

    Aggiunta array categorie con funzione

--- classes/seller.php
+++ classes/seller.php
@@ -3,6 +3,7 @@
 
     class Seller {
         public $sellerUsername;
+        public $productCategories = [];
         public $product;
 
         // constructor
@@ -15,13 +16,23 @@
         public function newProduct($sellerUsername, $productName, $category, $price) {
             $this->product = new Product($sellerUsername, $productName, $category, $price);
         }
+
+        public function setCategories($array) {
+            if(is_array($array)) {
+                $this->productCategories = $array;
+            } else {
+                $this->productCategories[] = $array;
+            }
+        }
         
     }
 
     $seller1 = new Seller("Shop1");
     $seller1->newProduct("Shop1", "Matita HB", "Cancelleria", 1.5);
+    $seller1->setCategories(["Cancelleria", "Giardinaggio", "Libri", "Elettronica"]);
     var_dump($seller1);
 
     $seller2 = new Seller("Shop 2");
     $seller2->newProduct("Shop2", "Padella 22", "Cucina", 25);
+    $seller2->setCategories(["Cucina", "Alimentari", "Abbigliamento"]);
     var_dump($seller2);
